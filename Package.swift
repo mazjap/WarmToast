cd8290c76d79e198468ef7ff8d2a71741ea79967
Jordan C <jordan.c4922@gmail.com>
2025-04-01 11:36:04 -0600

    Lower minimum tvOS version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "WarmToast",
     platforms: [
-        .iOS(.v13), .macOS(.v10_15), .tvOS(.v16)
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
